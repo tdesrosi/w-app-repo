a3e4d71c22d303a5e55f3605ef1cdb7f9a751bc7
lykasegura <lykasegura@google.com>
2022-08-04 22:15:34 +0000

    working validator

--- main.tf
+++ main.tf
@@ -1,8 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 3.84"
+    }
+  }
+}
+
 resource "google_project_iam_binding" "sample_iam_binding" {
   project = "w-appdev-0804"
   role    = "roles/viewer"
 
   members = [
-    "user:test@test.com"
+    "user@example.com"
   ]
 }
