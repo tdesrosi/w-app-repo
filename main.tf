1c0f1e6b48b8cf6a3764565231ea0b548f568f82
Lyka Segura <lykasegura@google.com>
2022-08-05 14:55:51 +0100

    testing failed scenario

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 3.84"
-    }
-  }
-}
-
 resource "google_project_iam_binding" "sample_iam_binding" {
   project = "w-appprod-0804"
   role    = "roles/viewer"
 
   members = [
-    "user@example.com"
+    "user:user@example.com"
   ]
 }
