71c19ab36e4858107261dcbbaf8b28f0135e9747
Lyka Segura <lykasegura@google.com>
2022-08-05 22:00:59 +0100

    final checkin before last test

--- backend.tf
+++ backend.tf
@@ -15,14 +15,8 @@
 # limitations under the License.
 
 
-# terraform {
-#   backend "gcs" {
-#     bucket = "w-demo-0809-dev-state-bucket"
-#   }
-# }
-
 terraform {
   backend "gcs" {
-    bucket = "w-appdev-0805-tfstate"
+    bucket = "w-demo-0809-dev-state-bucket"
   }
 }
