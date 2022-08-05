15492694690c32450aaad9c2a4f76331b61e0ec7
Lyka Segura <lykasegura@google.com>
2022-08-05 21:54:53 +0100

    testing constraints

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
+# FAILURE TEST SCENARIO
+
 resource "google_project_iam_binding" "sample_iam_binding" {
   project = "w-app-targetapp-prod-0805"
-  role    = "roles/compute.instances.setIamPolicy"
+  role    = "roles/compute.orgSecurityPolicyAdmin"
 
   members = [
     "user:user@example.com"
   ]
 }
 
+# SUCCESS SCENARIO
+
+# resource "google_storage_bucket" "test-storage-bucket" {
+# 	project = "w-app-targetapp-prod-0805"
+#   name          = "w-test-storage-0909"
+#   location      = "US"
+#   force_destroy = true
+# }
