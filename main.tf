e1bc77ece5a028d32a5158f65a8571ca47a5c69c
Lyka Segura <lykasegura@google.com>
2022-08-05 16:19:21 +0100

    retesting with git clone

--- main.tf
+++ main.tf
@@ -1,17 +1,39 @@
-terraform {
-  required_providers {
-    google = {
-      source = "hashicorp/google"
-      version = "~> 3.84"
+# resource "google_project_iam_binding" "sample_iam_binding" {
+#   project = "w-appprod-0804"
+#   role    = "roles/viewer"
+
+#   members = [
+#     "user:user@example.com"
+#   ]
+# }
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+  project = "w-appdev-0805"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-10"
     }
   }
+
+  network_interface {
+    network = "test-net"
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
 }
 
-resource "google_project_iam_binding" "sample_iam_binding" {
-  project = "w-appdev-0804"
-  role    = "roles/viewer"
-
-  members = [
-    "user:user@example.com"
-  ]
-}
+# resource "google_storage_bucket" "failbucket" {
+#   project =  "w-appdev-0805"
+#   name          = "aintworking-0805"
+#   location      = "EU"
+# }
