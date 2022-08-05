ec24d70cb89e0865566eb4b0b9a5d31a2a8725d8
Lyka Segura <lykasegura@google.com>
2022-08-05 15:01:40 +0100

    success scenario

--- main.tf
+++ main.tf
@@ -1,8 +1,37 @@
-resource "google_project_iam_binding" "sample_iam_binding" {
-  project = "w-appprod-0804"
-  role    = "roles/viewer"
+# resource "google_project_iam_binding" "sample_iam_binding" {
+#   project = "w-appprod-0804"
+#   role    = "roles/viewer"
 
-  members = [
-    "user:user@example.com"
-  ]
+#   members = [
+#     "user:user@example.com"
+#   ]
+# }
+
+resource "google_compute_instance" "default" {
+  name         = "test"
+  machine_type = "e2-medium"
+  zone         = "us-central1-a"
+
+  tags = ["foo", "bar"]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "SCSI"
+  }
+
+  network_interface {
+    network = "default"
+  }
+
+  metadata = {
+    foo = "bar"
+  }
+
+  metadata_startup_script = "echo hi > /test.txt"
 }
