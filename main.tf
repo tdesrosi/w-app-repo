7d42a8f4f0b5e020145a6b49294738795871f723
Lyka Segura <lykasegura@google.com>
2022-08-05 14:25:53 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "~> 3.84"
+    }
+  }
+}
+
+resource "google_project_iam_binding" "sample_iam_binding" {
+  project = "w-appdev-0804"
+  role    = "roles/viewer"
+
+  members = [
+    "user@example.com"
+  ]
+}
