14fae90dfaa2d91b95e7ab7efff096818e1cb631
Lyka Segura <lykasegura@google.com>
2022-08-08 21:23:39 +0100

    testing for demo

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 # FAILURE TEST SCENARIO
 
 # resource "google_project_iam_binding" "orgSecurityPolicy" {
-#   project = "w-targetapp-prod-0805"
+#   project = "w-app-targetapp-prod-0805"
 #   role    = "roles/compute.orgSecurityPolicyAdmin"
 
 #   members = [
@@ -10,7 +10,7 @@
 # }
 
 # resource "google_project_iam_binding" "editorRole" {
-#   project = "w-targetapp-prod-0805"
+#   project = "w-app-targetapp-prod-0805"
 #   role    = "roles/editor"
 
 #   members = [
@@ -19,7 +19,7 @@
 # }
 
 # resource "google_project_iam_binding" "bqAdmin" {
-#   project = "w-targetapp-prod-0805"
+#   project = "w-app-targetapp-prod-0805"
 #   role    = "roles/storage.admin"
 
 #   members = [
@@ -28,7 +28,7 @@
 # }
 
 # resource "google_project_iam_binding" "storageAdmin" {
-#   project = "w-targetapp-prod-0805"
+#   project = "w-app-targetapp-prod-0805"
 #   role    = "roles/storage.admin"
 
 #   members = [
@@ -38,7 +38,7 @@
 
 
 # resource "google_project_iam_binding" "projectIAMAdmin" {
-#   project = "w-targetapp-prod-0805"
+#   project = "w-app-targetapp-prod-0805"
 #   role    = "roles/resourcemanager.projectIamAdminn"
 
 #   members = [
@@ -46,31 +46,17 @@
 #   ]
 # }
 
-resource "google_project_iam_custom_role" "fail-cr" {
-  role_id     = "failCR"
-  title       = "This will fail"
-  description = "A description"
-  permissions = ["compute.firewallPolicies.setIamPolicy", "compute.images.list"]
-}
-
-
 # SUCCESS SCENARIO
 
-# resource "google_storage_bucket" "test-storage-bucket" {
-# 	project = "w-targetapp-prod-0805"
-#   name          = "w-test-storage-prd-0909"
-#   location      = "US"
-#   force_destroy = true
-# }
+resource "google_storage_bucket" "test-storage-bucket" {
+  project = "w-app-targetapp-prod-0805"
+  name          = "w-test-storage-prd-0909-test"
+  location      = "US"
+  force_destroy = true
+}
 
-
-# resource "google_service_account" "storageAdminSA" {
-#   account_id   = "w-targetapp-prod-0805"
-#   display_name = "sa-storage-admin"
-# }
-
-# resource "google_project_iam_member" "storageAdminSABinding" {
-#   project = "w-targetapp-prod-0805"
-#   role    = "roles/storage.admin"
-#   member  = "serviceAccount:id-w-targetprod-0805@w-appdev-0805.iam.gserviceaccount.com"
-# }
+resource "google_project_iam_member" "storageAdminSABinding" {
+  project = "w-app-targetapp-prod-0805"
+  role    = "roles/storage.admin"
+  member  = "demo-sa@w-app-targetapp-prod-0805.iam.gserviceaccount.com"
+}
