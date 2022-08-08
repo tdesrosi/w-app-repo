521d33597f914f651ec80fa986c010b621b48fc0
Lyka Segura <lykasegura@google.com>
2022-08-08 21:57:27 +0100

    testing for demo - success - dev

--- main.tf
+++ main.tf
@@ -1,19 +1,62 @@
 # FAILURE TEST SCENARIO
 
-resource "google_project_iam_binding" "sample_iam_binding" {
-  project = "w-app-targetapp-dev-0805"
-  role    = "roles/compute.orgSecurityPolicyAdmin"
+# resource "google_project_iam_binding" "orgSecurityPolicy" {
+#   project = "w-app-targetapp-dev-0805"
+#   role    = "roles/compute.orgSecurityPolicyAdmin"
 
-  members = [
-    "user:lyka@lykasegura.joonix.net"
-  ]
-}
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
+
+# resource "google_project_iam_binding" "editorRole" {
+#   project = "w-app-targetapp-dev-0805"
+#   role    = "roles/editor"
+
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
+
+# resource "google_project_iam_binding" "bqAdmin" {
+#   project = "w-app-targetapp-dev-0805"
+#   role    = "roles/storage.admin"
+
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
+
+# resource "google_project_iam_binding" "storageAdmin" {
+#   project = "w-app-targetapp-dev-0805"
+#   role    = "roles/storage.admin"
+
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
+
+
+# resource "google_project_iam_binding" "projectIAMAdmin" {
+#   project = "w-app-targetapp-dev-0805"
+#   role    = "roles/resourcemanager.projectIamAdminn"
+
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
 
 # SUCCESS SCENARIO
 
-# resource "google_storage_bucket" "test-storage-bucket" {
-# 	project = "w-app-targetapp-dev-0805"
-#   name          = "w-test-storage-0909"
-#   location      = "US"
-#   force_destroy = true
-# }
+resource "google_storage_bucket" "test-storage-bucket" {
+	project = "w-app-targetapp-dev-0805"
+  name          = "w-test-storage-0909"
+  location      = "US"
+  force_destroy = true
+}
+
+resource "google_project_iam_member" "storageAdminSABinding" {
+  project = "w-app-targetapp-dev-0805"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:demo-sa@w-app-targetapp-dev-0805.iam.gserviceaccount.com"
+}
