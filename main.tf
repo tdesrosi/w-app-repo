ecfec54096ff11820be11d850adf4dd5355e4c64
Lyka Segura <lykasegura@google.com>
2022-08-08 15:44:06 +0100

    testing scenarios

--- main.tf
+++ main.tf
@@ -27,14 +27,14 @@
 #   ]
 # }
 
-resource "google_project_iam_binding" "projectIAMAdmin" {
-  project = "w-targetapp-prod-0805"
-  role    = "roles/resourcemanager.projectIamAdminn"
+# resource "google_project_iam_binding" "projectIAMAdmin" {
+#   project = "w-targetapp-prod-0805"
+#   role    = "roles/resourcemanager.projectIamAdminn"
 
-  members = [
-    "user:lyka@lykasegura.joonix.net"
-  ]
-}
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
 
 
 # SUCCESS SCENARIO
@@ -46,14 +46,14 @@ resource "google_project_iam_binding" "projectIAMAdmin" {
 #   force_destroy = true
 # }
 
-# resource "google_project_iam_binding" "storageAdmin" {
-#   project = "w-targetapp-prod-0805"
-#   role    = "roles/storage.admin"
+resource "google_project_iam_binding" "storageAdmin" {
+  project = "w-targetapp-prod-0805"
+  role    = "roles/storage.admin"
 
-#   members = [
-#     "user:lyka@lykasegura.joonix.net"
-#   ]
-# }
+  members = [
+    "user:lyka@lykasegura.joonix.net"
+  ]
+}
 
 # resource "google_service_account" "storageAdminSA" {
 #   account_id   = "w-targetapp-prod-0805"
