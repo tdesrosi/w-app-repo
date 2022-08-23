2bdd669244bc53dfeb8e807b5e1372bebf198689
Lyka Segura <lykasegura@google.com>
2022-08-23 15:46:56 +0100

    ready for demo

--- main.tf
+++ main.tf
@@ -1,62 +1,62 @@
 # FAILURE TEST SCENARIO
 
-resource "google_project_iam_binding" "orgSecurityPolicy" {
-  project = "w-app-targetapp-prod-0805"
-  role    = "roles/compute.orgSecurityPolicyAdmin"
+# resource "google_project_iam_binding" "orgSecurityPolicy" {
+#   project = "w-app-targetapp-prod-0805"
+#   role    = "roles/compute.orgSecurityPolicyAdmin"
 
-  members = [
-    "user:lyka@lykasegura.joonix.net"
-  ]
-}
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
 
-resource "google_project_iam_binding" "editorRole" {
-  project = "w-app-targetapp-prod-0805"
-  role    = "roles/editor"
+# resource "google_project_iam_binding" "editorRole" {
+#   project = "w-app-targetapp-prod-0805"
+#   role    = "roles/editor"
 
-  members = [
-    "user:lyka@lykasegura.joonix.net"
-  ]
-}
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
 
-resource "google_project_iam_binding" "bqAdmin" {
-  project = "w-app-targetapp-prod-0805"
-  role    = "roles/storage.admin"
+# resource "google_project_iam_binding" "bqAdmin" {
+#   project = "w-app-targetapp-prod-0805"
+#   role    = "roles/storage.admin"
 
-  members = [
-    "user:lyka@lykasegura.joonix.net"
-  ]
-}
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
 
-resource "google_project_iam_binding" "storageAdmin" {
-  project = "w-app-targetapp-prod-0805"
-  role    = "roles/storage.admin"
+# resource "google_project_iam_binding" "storageAdmin" {
+#   project = "w-app-targetapp-prod-0805"
+#   role    = "roles/storage.admin"
 
-  members = [
-    "user:lyka@lykasegura.joonix.net"
-  ]
-}
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
 
 
-resource "google_project_iam_binding" "projectIAMAdmin" {
-  project = "w-app-targetapp-prod-0805"
-  role    = "roles/resourcemanager.projectIamAdminn"
+# resource "google_project_iam_binding" "projectIAMAdmin" {
+#   project = "w-app-targetapp-prod-0805"
+#   role    = "roles/resourcemanager.projectIamAdminn"
 
-  members = [
-    "user:lyka@lykasegura.joonix.net"
-  ]
-}
+#   members = [
+#     "user:lyka@lykasegura.joonix.net"
+#   ]
+# }
 
 # SUCCESS SCENARIO
 
-# resource "google_storage_bucket" "test-storage-bucket" {
-#   project = "w-app-targetapp-prod-0805"
-#   name          = "w-test-storage-prd-demo-0823"
-#   location      = "US"
-#   force_destroy = true
-# }
+resource "google_storage_bucket" "test-storage-bucket" {
+  project = "w-app-targetapp-prod-0805"
+  name          = "w-test-storage-prd-demo-0823"
+  location      = "US"
+  force_destroy = true
+}
 
-# resource "google_project_iam_member" "storageAdminSABinding" {
-#   project = "w-app-targetapp-prod-0805"
-#   role    = "roles/storage.admin"
-#   member  = "serviceAccount:demo-sa@w-app-targetapp-prod-0805.iam.gserviceaccount.com"
-# }
+resource "google_project_iam_member" "storageAdminSABinding" {
+  project = "w-app-targetapp-prod-0805"
+  role    = "roles/storage.admin"
+  member  = "serviceAccount:demo-sa@w-app-targetapp-prod-0805.iam.gserviceaccount.com"
+}
