b4016792d6c681184d7bd220e1d487be13becf49
Thomas Desrosiers <tdesrosi@google.com>
2022-11-02 17:51:02 -0400

    something

--- main.tf
+++ main.tf
@@ -69,9 +69,3 @@ resource "google_project_iam_custom_role" "my-custom-role" {
   description = "A description"
   permissions = ["iam.roles.list", "iam.roles.create", "iam.roles.delete"]
 }
-
-resource "google_project_iam_member" "customRoleBindingForSA" {
-  project = "w-secteam-target-prod"
-  role    = "w-secteam-target-prod/roles/myCustomRole"
-  member  = "serviceAccount:dev-env@w-secteam-app-pipeline.iam.gserviceaccount.com"
-}
